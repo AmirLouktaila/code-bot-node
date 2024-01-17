cb1513186f66a5b9ef70d546591c1915074c14e5
Salah lkt <amir.louktila@gmail.com>
2024-01-17 04:24:24 -1200

    coosk

--- index.js
+++ index.js
@@ -154,11 +154,12 @@ bot.on('text', async (ctx) => {
                             const links = extractLinks(`${ctx.message.text}`)
 
                             if (links[0].includes("/p/trade/confirm.html")) {
+
                                 const match = links[0].match(/availableProductShopcartIds=([\d,]+)/);
 
                                 if (match) {
                                     let numbersText = match[1];
-                                    numbersText = numbersText.replace(',', '%2C');
+                                    numbersText = numbersText.replaceAll(',', '%2C');
                                     const finalUrl = `https://www.aliexpress.com/p/trade/confirm.html?availableProductShopcartIds=${numbersText}&extraParams=%7B%22channelInfo%22%3A%7B%22sourceType%22%3A%22620%22%7D%7D&aff_fcid=`;
                                     console.log(finalUrl);
                                     try {
