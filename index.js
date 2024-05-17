f01328884d122e44bed2bfbf664e5aa9a3802c76
Salah Louktaila <86385687+AmirLouktaila@users.noreply.github.com>
2024-05-17 22:35:24 +0300

    Uste index.js

--- index.js
+++ index.js
@@ -191,19 +191,19 @@ bot.on('text', async (ctx) => {
                                     aliExpressLib.getData(response_link)
                                         .then((coinPi) => {
                                             console.log("coinPi : ", coinPi)
-                                            let couponList = "";
+                                            // let couponList = "";
 
-                                            if (coinPi.info.normal.coupon == "لا يوجد كوبونات ❎") {
-                                                couponList = coinPi.info.normal.coupon;
-                                            } else {
-                                                couponList = "";
-                                                coinPi.info.normal.coupon.forEach(coupons => {
-                                                    const code = coupons.code;
-                                                    const detail = coupons.detail.replace('طلبات تزيد على US ', '');
-                                                    const desc = coupons.desc.replace('US ', '');
-                                                    couponList += `🎁${desc}/${detail} :${code}\n`;
-                                                });
-                                            }
+                                            // if (coinPi.info.normal.coupon == "لا يوجد كوبونات ❎") {
+                                            //     couponList = coinPi.info.normal.coupon;
+                                            // } else {
+                                            //     couponList = "";
+                                            //     coinPi.info.normal.coupon.forEach(coupons => {
+                                            //         const code = coupons.code;
+                                            //         const detail = coupons.detail.replace('طلبات تزيد على US ', '');
+                                            //         const desc = coupons.desc.replace('US ', '');
+                                            //         couponList += `🎁${desc}/${detail} :${code}\n`;
+                                            //     });
+                                            // }
                                          let total;
 if (coinPi.info.points.discount != 'لا توجد نسبة تخفيض بالعملات ❎') {
     var dise = coinPi.info.points.discount.replace("خصم النقاط ", "");
@@ -259,7 +259,6 @@ ${coinPi.aff.super}
 📌رابط العرض المحدود: ${coinPi.info.limited.price}
 ${coinPi.aff.limited}
 <b>----------- | ✨ الكوبونات ✨ | -----------</b>
-${couponList}
 ` ,
                                                     parse_mode: "HTML",
                                                     ...Markup.inlineKeyboard([
